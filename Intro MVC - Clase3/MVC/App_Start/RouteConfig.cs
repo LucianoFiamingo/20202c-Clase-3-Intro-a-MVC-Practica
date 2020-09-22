3c7b0821348eb2645577ff129486d9347ae87508
lucho <lucianofiamingo@gmail.com>
2020-09-22 16:56:14 -0300

    Bienvenido

--- Intro MVC - Clase3/MVC/App_Start/RouteConfig.cs
+++ Intro MVC - Clase3/MVC/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace MVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Bienvenidos",
+                url: "Bienvenido/{action}/{nombre1}/{nombre2}",
+                defaults: new { controller = "Bienvenido", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Bienvenido", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Intro MVC - Clase3/MVC/Controllers/BienvenidoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class BienvenidoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Saludo(string id)
+        {
+            return $"HOLA {id}!";
+        }
+
+        public string SaludoGrupal(string nombre1, string nombre2)
+        {
+            return $"HOLA {nombre1} y {nombre2}!";
+        }
+
+    }
+}

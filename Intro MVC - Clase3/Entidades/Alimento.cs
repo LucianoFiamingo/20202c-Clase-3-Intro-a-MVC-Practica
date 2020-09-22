52c7738975b64d67010259620933a73417b4232d
lucho <lucianofiamingo@gmail.com>
2020-09-22 15:44:28 -0300

    alimento Entidad

---
+++ Intro MVC - Clase3/Entidades/Alimento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Alimento
+    {
+        public Guid UniqueID { get; set; }
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
